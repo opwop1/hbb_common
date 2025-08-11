a3b9d89bac7715b20b9aab5f2804e11f44cc94b7
unknown <slhp1013@qq.com>
2025-08-11 14:30:04 +0800

    修改服务器地址和key

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rdapi.wanfengvtc.top"];
+pub const RS_PUB_KEY: &str = "McVW3B8bKdy56hB07pAvBrnM+ZbDdJg+Qd9Gg0v4Mqk=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
